622a0bfea5448b7f4c11bfa3adfbed44962ed39d
Dennis <dennis.o.arroyo@gmail.con>
2018-11-05 18:28:22 -0400

    Second Commit

---
+++ Practice/TuplesAndList.py
@@ -0,0 +1,17 @@
+lst = [1,2,3,4,"Dennis"]
+
+print("\nList:\n")
+print(lst)
+
+for n in lst:
+    print("\n", n)
+
+tpl = (1,2,3,4,5)
+tpl2 = (6,7,8,9,10)
+
+print("\nTuple #1: ", tpl)
+print()
+print("Tuple #2: ", tpl2)
+
+print()
+print("Tuple #1 + Tuple #2: ", tpl + tpl2)
