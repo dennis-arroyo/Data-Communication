5b3c4cb25685f72a682d57367bc49d82de28486a
Dennis <dennis.o.arroyo@gmail.con>
2018-11-24 23:39:39 -0400

    Kivy and other socket method

---
+++ Practice/Server_02.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = '127.0.0.1'
+
+PORT = 7575
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    connection, address = s.accept()
+
+    with connection:
+        print('Connected by', address)
+
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data)
+
