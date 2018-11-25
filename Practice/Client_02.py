8fee76d189280783bfc39698bbebdd933640ea6b
Dennis <dennis.o.arroyo@gmail.con>
2018-11-25 01:16:32 -0400

    Kivy and other socket method

--- Practice/Client_02.py
+++ Practice/Client_02.py
@@ -1,12 +1,28 @@
 import socket
 
-HOST = '127.0.0.1'
+# HOST = '127.0.0.1'
 
+HOST = socket.gethostbyname("")
 PORT = 7575
+
+print("Choose what to send: ")
+
+print("(1) Hello World")
+print("(2) Close")
+
+answer = input()
+
+if answer is '1':
+    msg = 'Hello World'
+elif answer is '2':
+    msg = 'Close'
+else:
+    msg = "Default"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.send(b'Hello World')
+    # s.send(b'Hello Dennis')
+    s.send(msg.encode())
     data = s.recv(1024)
 
-print('Received', repr(data))
+print('Received:', repr(data))
--- Practice/Server_02.py
+++ Practice/Server_02.py
@@ -1,20 +1,34 @@
 import socket
 
-HOST = '127.0.0.1'
+# HOST = '127.0.0.1'
+
+HOST = socket.gethostbyname("")
 
 PORT = 7575
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    connection, address = s.accept()
 
-    with connection:
-        print('Connected by', address)
+    # connection, address = s.accept()   ####################################################
+#                                        If this is implemented outside the while True,
+    # with connection:                   it only listens for one connection and ends program.
+    #     print('Connected by', address) ####################################################
 
-        while True:
+    while True:
+        connection, address = s.accept()
+
+        with connection:
+            print('Connected by', address)
+
             data = connection.recv(1024)
+
             if not data:
                 break
+
+            print(data.decode())
             connection.sendall(data)
 
+            if data.decode() == "Close":
+                print("Closing program...")
+                break
