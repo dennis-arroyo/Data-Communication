b7124ea94ef6d54b250f8a1880ef69ac1cf175ee
Dennis <dennis.o.arroyo@gmail.con>
2018-12-10 00:00:05 -0400

    coronando el ganador

--- Dennis/Server/server_socket.py
+++ Dennis/Server/server_socket.py
@@ -19,12 +19,16 @@ clients = {}
 text_field = "Players \t\tScore \t\tPosition\n\n"
 player = ""
 score = "0"
+min_seconds = ["", "99999999999"]
 
 
 def client_thread(conn, add, cl):
     while True:
 
         global score
+        global min_seconds
+
+        print("Real Address: " + add)
 
         data = conn.recv(1024)
         reply = data.decode()
@@ -32,8 +36,21 @@ def client_thread(conn, add, cl):
         if not data:
             break
         if reply == "get_score":
-            cl[add][1] = conn.recv(1024).decode()
+            seconds = conn.recv(1024).decode()
+            cl[add][1] = seconds
+            if seconds < min_seconds[1]:
+                min_seconds = []
+                min_seconds.append(add)
+                min_seconds.append(seconds)
+                print("Max Score Address: " + min_seconds[0])
+
         else:
+
+            if add is min_seconds[0]:
+                cl[add][2] = "winning"
+            else:
+                cl[add][2] = "losing"
+
             extracted_players = [value for key, value in cl.items()]
 
             players = ""
