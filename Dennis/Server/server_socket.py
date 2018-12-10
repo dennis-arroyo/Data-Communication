d7556ea56320a6989a11d8e9293421366dc18c58
Dennis <dennis.o.arroyo@gmail.con>
2018-12-09 20:04:45 -0400

    working

--- Dennis/Server/server_socket.py
+++ Dennis/Server/server_socket.py
@@ -1,4 +1,5 @@
 import socket
+import random
 from _thread import *
 
 # HOST = '127.0.0.1'
@@ -27,7 +28,6 @@ def client_thread(conn, add, cl):
 
         data = conn.recv(1024)
         reply = data.decode()
-        print(type(reply))
         # print(cl[add])
         if not data:
             break
@@ -38,37 +38,36 @@ def client_thread(conn, add, cl):
 
             players = ""
 
-            # for pl in extracted_players:
-            #     players += pl + ","
-
             for p in extracted_players:
                 players += p[0] + "\t\t" + p[1] + "\t\t" + p[2] + "\n"
 
             conn.sendall(players.encode())
 
 
-def other_clients(cl, current_address):
-    addresses = [key for key, value in cl.items() if key not in current_address]
-    print("\t\t", addresses)
-    ip_and_port = []
+def get_numbers(orientation):
+    numbers = str(random.randrange(orientation))
 
-    for a in addresses:
-        ip, p = a.split(":")
-        ip_and_port.append([ip, int(p)])
+    counter = 0
 
-    return ip_and_port
+    while counter < 5:
+        numbers += "," + str(random.randrange(orientation))
+        counter += 1
+
+    return numbers
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
+    x = get_numbers(475)
+    y = get_numbers(650)
+
     while True:
         obj = s.accept()
         connection, address = obj
         player = connection.recv(1024)
-        # print(player.decode())
+        connection.sendall((x + "-" + y).encode())
         concatenated_address = address[0] + ":" + str(address[1])
         clients[concatenated_address] = [player.decode(), "0", "null"]
-        # print(clients)
         start_new_thread(client_thread, (connection, concatenated_address, clients))
